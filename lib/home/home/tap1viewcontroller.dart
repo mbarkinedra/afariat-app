348b03199893087cf6f48da43fc6aa236317a119
Nedra-Mbarki <nedrambarki1995@github.com>
2021-12-01 16:02:49 +0100

    ajouter la squlette de l 'application

---
+++ lib/home/home/tap1viewcontroller.dart
@@ -0,0 +1,11 @@
+
+import 'package:get/get.dart';
+
+
+class Tap1ViewController extends GetxController {
+
+
+
+
+
+}
---
+++ lib/home/tap5/tap5_scr.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'tap5viewcontroller.dart';
+
+class Tap5 extends GetWidget<Tab5> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
