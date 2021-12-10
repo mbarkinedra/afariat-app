477a0cf55fe4d37e69cb89fcd46d15598d426cfa
skander <mabroukskander@gmail.com>
2021-12-10 22:15:20 +0100

    Add error validation in register form

--- lib/sign_up/sign_up_viewcontroller.dart
+++ lib/sign_up/sign_up_viewcontroller.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:afariat/controllers/loc_controller.dart';
@@ -10,12 +9,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignUpViewController extends GetxController {
+  final registerFormKey = GlobalKey<FormState>();
   final tapHomeViewController = Get.find<LocController>();
   TextEditingController name = TextEditingController();
   TextEditingController password = TextEditingController();
   TextEditingController phone = TextEditingController();
-
   TextEditingController email = TextEditingController();
+
+  bool isVisiblePassword = false;
+
   SignUpApi _signUpApi = SignUpApi();
   List<TypeRegister> typeList = [
     TypeRegister(name: "Particulier", id: 0),
@@ -23,16 +25,44 @@ class SignUpViewController extends GetxController {
   ];
   TypeRegister type;
 
+  Map<String, dynamic> serverErrors;
+
+  /// value: is the entered user value, field: is the name of the field
+  String validator(String value, String field) {
+    //1st validate the front entered fields, then validate the errors from server
+    //
+    String errorMessage = null;
+    //validating server errors
+    serverErrors.forEach((key, elementErrors) {
+      if (field == key && elementErrors.containsKey('errors')) {
+        if (elementErrors['errors'].length > 0) {
+          errorMessage = elementErrors['errors'][0];
+        }
+      }
+    });
+
+    return errorMessage ?? null;
+  }
+
+  void showHidePassword() {
+    this.isVisiblePassword = !this.isVisiblePassword;
+    print('pressed');
+
+    update();
+  }
+
   @override
   void onInit() {
     super.onInit();
     type = typeList[0];
   }
 
+
   updateTypeRegister(TypeRegister t) {
     type = t;
     update();
   }
+
 /*  validatorfun(String  v){
     if (v == null || v.isEmpty) {
 
@@ -55,40 +85,44 @@ class SignUpViewController extends GetxController {
 
       switch (value.statusCode) {
         case 201:
-        //Affichage success
-        //Map<String,dynamic> data=jsonDecode(value.data);
-        print("Message à écrire: ${value.data['message']}");
+          //Affichage success
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => SignUpSucces(value.data['message'])),
+            MaterialPageRoute(
+                builder: (context) => SignUpSucces(value.data['message'])),
           );
           break;
         case 400:
+          serverErrors = value.data;
+          value.data.forEach((key, value) {
+            print('Key: $key');
+            print('------------------------------');
+          });
+          registerFormKey.currentState.validate();
 
-//           print(value.data);
+          //validator('email');
 //           Error_Register errors= Error_Register.fromJson(  value.data);
 //           print(errors);
 // var a=value.data ;
 
-          Get.snackbar("error", "${ value.data}");
+          Get.snackbar(
+            'Erreur',
+            'Veuillez corriger les erreurs ci-dessous.',
+            colorText: Colors.white,
+            backgroundColor: Colors.red,
+          );
           break;
         default:
           return;
       }
-     // Navigator.pushReplacement(
-     //    context,
-     //    MaterialPageRoute(builder: (context) => SignUpSucces()),
-     //  );
-    }).catchError((e){
-
-
-     // Error_Register errors= Error_Register.fromJson(  e);
-      Get.snackbar("error", "${ e }");
-
-
-
+      // Navigator.pushReplacement(
+      //    context,
+      //    MaterialPageRoute(builder: (context) => SignUpSucces()),
+      //  );
+    }).catchError((e) {
+      // Error_Register errors= Error_Register.fromJson(  e);
+      Get.snackbar("error", "${e}");
     });
-   // print("response.statusCode ${response.statusCode}");
-
+    // print("response.statusCode ${response.statusCode}");
   }
 }
